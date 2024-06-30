8bf714c2e27152386cb1721dc17ef822f252df1c
Victor Parizio <victorparizio@hotmail.com>
2024-06-29 22:46:15 -0300

    feat: atualização da thumbnail

--- dist/routes/productRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const product_1 = require("../controllers/product");
-const route = (0, express_1.Router)();
-route.get("/produto", product_1.getProducts);
-exports.default = route;
-//# sourceMappingURL=productRouter.js.map
--- dist/routes/testimonialRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const testimonial_1 = require("../controllers/testimonial");
-const route = (0, express_1.Router)();
-route.get("/testimonial", testimonial_1.listTestimonial);
-exports.default = route;
-//# sourceMappingURL=testimonialRouter.js.map
