dad93d82d99f3a8afc6f9f2dac333a682ce12491
VictorParizio <victorparizio@hotmail.com>
2025-11-03 11:18:34 -0300

    fix: swagger

--- src/routes/swaggerRouter.ts
+++ src/routes/swaggerRouter.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-const route = Router();
-
-import swaggerUI from "swagger-ui-express";
+import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
 
-route.use("/", swaggerUI.serve);
-route.get("/", swaggerUI.setup(swaggerDocument));
+const router = Router();
 
-export default route
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+export default router;
